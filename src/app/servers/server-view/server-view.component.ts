bebefc9ea2d06cd189a978a9626e6c0cfeb881da
GautamYadav19 <gautamyadav1992002@gmail.com>
2022-09-27 01:15:22 +0530

    minor update

--- src/app/servers/server-view/server-view.component.ts
+++ src/app/servers/server-view/server-view.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersInformation } from '../../share/users';
+import { ServerService } from '../server-services';
 
 @Component({
   selector: 'app-server-view',
   templateUrl: './server-view.component.html',
-  styleUrls: ['./server-view.component.css']
+  styleUrls: ['./server-view.component.css'],
 })
 export class ServerViewComponent implements OnInit {
+  servers!: UsersInformation[];
+  constructor(private services: ServerService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.servers = this.services.getUserInformation();
   }
-
 }
